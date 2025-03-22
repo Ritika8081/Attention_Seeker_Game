cf3c5d1ff29c40b434490de5fcdfa4e41de056f5
Ritika Mishra <ritikamis@gmail.com>
2025-03-22 10:09:54 +0530

    implemented eeg and emg to play game

---
+++ script.js
@@ -0,0 +1,9 @@
+// Handle Start Button Click
+document.getElementById("startGame").addEventListener("click", function() {
+    window.location.href = "game.html"; // Redirect to the game page
+});
+
+// Handle Exit Button Click
+document.getElementById("exitGame").addEventListener("click", function() {
+    window.close(); // Attempt to close the window (may not work on all browsers)
+});
