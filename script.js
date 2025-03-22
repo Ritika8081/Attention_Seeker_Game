ab0a6b839ad2fc6fef657113b66d0981743da541
Ritika Mishra <ritikamis@gmail.com>
2025-03-22 17:22:05 +0530

    updated file position and naming

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Handle Start Button Click
 document.getElementById("startGame").addEventListener("click", function() {
-    window.location.href = "index.html"; // Redirect to the game page
+    window.location.href = "game.html"; // Redirect to the game page
 });
 
 // Handle Exit Button Click
