d5c0053e138d0880222f7d352733e8e8f73dbe28
Ritika Mishra <ritikamis@gmail.com>
2025-07-14 14:57:12 +0530

    fixed issue of image loading and resize on screen change

--- script.js
+++
@@ -1,9 +0,0 @@
-// Handle Start Button Click
-document.getElementById("startGame").addEventListener("click", function() {
-    window.location.href = "game.html"; // Redirect to the game page
-});
-
-// Handle Exit Button Click
-document.getElementById("exitGame").addEventListener("click", function() {
-    window.close(); // Attempt to close the window (may not work on all browsers)
-});
